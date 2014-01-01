bfa78e07ff0f1090ab79cc4e16a27feaea51bbcf
porwat <piotr@orwat.info>
2014-01-01 20:00:33 +0100

    one major change

--- CleaningDirectory.ps1
+++ CleaningDirectory.ps1
@@ -3,4 +3,4 @@
 $Path = "Path_To_Dir"
 
 $a = get-date (get-date).AddMonths(-1)
-Get-ChildItem -Path $Path | Where {$_.Attributes -EQ "Directory" -And $_.CreationTime -gt $a} | Remove-Item -Recurse -WhatIf
+Get-ChildItem -Path $Path | Where {$_.Attributes -EQ "Directory" -And $_.CreationTime -LT $a} | Remove-Item -Recurse -WhatIf
