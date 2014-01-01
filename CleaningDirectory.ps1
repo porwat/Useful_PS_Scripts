c89f010bc6f7664764e9fae7f517d63c8b7c1817
porwat <piotr@orwat.info>
2014-01-01 19:55:14 +0100

    some minor changes

--- CleaningDirectory.ps1
+++ CleaningDirectory.ps1
@@ -1,4 +1,6 @@
-$Path = "C:\Users\Piotr\Desktop\TestDir"
+#This script remove all directories older than a month
+
+$Path = "Path_To_Dir"
 
 $a = get-date (get-date).AddMonths(-1)
 Get-ChildItem -Path $Path | Where {$_.Attributes -EQ "Directory" -And $_.CreationTime -gt $a} | Remove-Item -Recurse -WhatIf
