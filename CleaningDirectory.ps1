134d017252c25ebc1ea60051134c573b39cf0dde
porwat <piotr@orwat.info>
2014-01-02 21:43:14 +0100

    Added a few improvements

--- CleaningDirectory.ps1
+++ CleaningDirectory.ps1
@@ -1,6 +1,7 @@
 #This script remove all directories older than a month
 
-$Path = "Path_To_Dir"
+$Path = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition #Get current path
+$Exclusion = "Dir1", "Dir2"
 
 $a = get-date (get-date).AddMonths(-1)
-Get-ChildItem -Path $Path | Where {$_.Attributes -EQ "Directory" -And $_.CreationTime -LT $a} | Remove-Item -Recurse -WhatIf
+Get-ChildItem -Path $Path | Where {$_.Attributes -EQ "Directory" -And $_.CreationTime -LT $a -And $_.Name -and ($Exclusion -notcontains $_.Name) } | Remove-Item -Recurse -WhatIf
