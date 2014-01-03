6ffd5d652bb1a3751563cc290a2f13836e3cc20f
porwat <piotr@orwat.info>
2014-01-03 07:40:48 +0100

    Update CleaningDirectory.ps1

--- CleaningDirectory.ps1
+++ CleaningDirectory.ps1
@@ -4,4 +4,4 @@ $Path = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition #Get current
 $Exclusion = "Dir1", "Dir2"
 
 $a = get-date (get-date).AddMonths(-1)
-Get-ChildItem -Path $Path | Where {$_.Attributes -EQ "Directory" -And $_.CreationTime -LT $a -And $_.Name -and ($Exclusion -notcontains $_.Name) } | Remove-Item -Recurse -WhatIf
+Get-ChildItem -Path $Path | Where {$_.Attributes -EQ "Directory" -And $_.CreationTime -LT $a -And $_.Name -and ($Exclusion -notcontains $_.Name) } | Remove-Item -Recurse -WhatIf #Delete -WhatIf if you want to allow this script to delete found folders
